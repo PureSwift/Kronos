a45f9edeb607daa14581e5b9c48f30d3b63147fd
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-01-23 18:09:23 -0500

    added StencilFunction

---
+++ Sources/SwiftGL/StencilFuncion.swift
@@ -0,0 +1,19 @@
+//
+//  StencilFuncion.swift
+//  SwiftGL
+//
+//  Created by Alsey Coleman Miller on 1/23/16.
+//  Copyright © 2016 PureSwift. All rights reserved.
+//
+
+public enum StencilFunction: GLenum {
+    
+    case Never              = 0x0200
+    case Less               = 0x0201
+    case Equal              = 0x0202
+    case LessThanOrEqual    = 0x0203
+    case Greater            = 0x0204
+    case NotEqual           = 0x0205
+    case GreaterThanOrEqual = 0x0206
+    case Always             = 0x0207
+}
