06382b2398aee1470bf3e8d860dce6e7a430afc8
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-01-24 15:25:46 -0500

    added Math extensions to Vector3

--- Sources/Kronos/Vector3.swift
+++ Sources/Kronos/Vector3.swift
@@ -92,11 +92,190 @@ public struct Vector3: OpenGLMathType {
 // MARK: - Equatable
 
 @inline(__always)
-public func ==(lhs: Vector3, rhs: Vector3) -> Bool {
+public func == (lhs: Vector3, rhs: Vector3) -> Bool {
     
     return lhs.value.0  == rhs.value.0
         && lhs.value.1  == rhs.value.1
         && lhs.value.2  == rhs.value.2
+}
+
+@inline(__always)
+public func == (lhs: Vector3, rhs: Float) -> Bool {
+    
+    return lhs.value.0  == rhs
+        && lhs.value.1  == rhs
+        && lhs.value.2  == rhs
+}
+
+// MARK: - Comparable
+
+@inline(__always)
+public func < (lhs: Vector3, rhs: Vector3) -> Bool {
+    
+    return lhs.value.0  < rhs.value.0
+        && lhs.value.1  < rhs.value.1
+        && lhs.value.2  < rhs.value.2
+}
+
+@inline(__always)
+public func < (lhs: Vector3, rhs: Float) -> Bool {
+    
+    return lhs.value.0  < rhs
+        && lhs.value.1  < rhs
+        && lhs.value.2  < rhs
+}
+
+// MARK: - Math
+
+/// Negate
+@inline(__always)
+public prefix func - (vector: Vector3) -> Vector3 {
+    
+    return Vector3( -vector.value.0, -vector.value.1, -vector.value.2)
+}
+
+/// Add
+@inline(__always)
+public func + (lhs: Vector3, rhs: Vector3) -> Vector3 {
+    
+    return Vector3(lhs.value.0 + rhs.value.0,
+        lhs.value.1 + rhs.value.1,
+        lhs.value.2 + rhs.value.2)
+}
+
+/// Subtract
+@inline(__always)
+public func - (lhs: Vector3, rhs: Vector3) -> Vector3 {
+    
+    return Vector3(lhs.value.0 - rhs.value.0,
+        lhs.value.1 - rhs.value.1,
+        lhs.value.2 - rhs.value.2)
+}
+
+/// Multiply
+@inline(__always)
+public func * (lhs: Vector3, rhs: Vector3) -> Vector3 {
+    
+    return Vector3(lhs.value.0 * rhs.value.0,
+        lhs.value.1 * rhs.value.1,
+        lhs.value.2 * rhs.value.2)
+}
+
+/// Divide
+@inline(__always)
+public func / (lhs: Vector3, rhs: Vector3) -> Vector3 {
+    
+    return Vector3(lhs.value.0 / rhs.value.0,
+        lhs.value.1 / rhs.value.1,
+        lhs.value.2 / rhs.value.2)
+}
+
+/// Add Scalar
+@inline(__always)
+public func + (lhs: Vector3, rhs: Float) -> Vector3 {
+    
+    return Vector3(lhs.value.0 + rhs,
+        lhs.value.1 + rhs,
+        lhs.value.2 + rhs)
+}
+
+/// Subtract Scalar
+@inline(__always)
+public func - (lhs: Vector3, rhs: Float) -> Vector3 {
+    
+    return Vector3(lhs.value.0 - rhs,
+        lhs.value.1 - rhs,
+        lhs.value.2 - rhs)
+}
+
+/// Multiply Scalar
+@inline(__always)
+public func * (lhs: Vector3, rhs: Float) -> Vector3 {
+    
+    return Vector3(lhs.value.0 * rhs,
+        lhs.value.1 * rhs,
+        lhs.value.2 * rhs)
+}
+
+/// Divide Scalar
+@inline(__always)
+public func / (lhs: Vector3, rhs: Float) -> Vector3 {
+    
+    return Vector3(lhs.value.0 / rhs, lhs.value.1 / rhs, lhs.value.2 / rhs)
+}
+
+public extension Vector3 {
+    
+    @inline(__always)
+    func maximum(rhs: Vector3) -> Vector3 {
+        
+        var max = self
+        
+        if rhs.value.0 > value.0 {
+            max.value.0 = value.0
+        }
+        if rhs.value.1 > value.1 {
+            max.value.1 = value.1
+        }
+        if rhs.value.2 > value.2 {
+            max.value.2 = value.2
+        }
+        
+        return max
+    }
+    
+    @inline(__always)
+    func minimum(rhs: Vector3) -> Vector3 {
+        
+        var min = self
+        
+        if rhs.value.0 < value.0 {
+            min.value.0 = value.0
+        }
+        if rhs.value.1 < value.1 {
+            min.value.1 = value.1
+        }
+        if rhs.value.2 < value.2 {
+            min.value.2 = value.2
+        }
+        
+        return min
+    }
+    
+    @inline(__always)
+    func normalize() -> Vector3 {
+        
+        let scale = Float(1.0) / self.length
+        let vector = self * scale
+        return vector
+    }
+    
+    @inline(__always)
+    func dotProduct(rhs: Vector3) -> Float {
+        
+        return value.0 * rhs.value.0 + value.1 * rhs.value.1 + value.2 * rhs.value.2
+    }
+    
+    var length: Float {
+        
+        return sqrt(value.0 * value.0 + value.1 * value.1 + value.2 * value.2)
+    }
+    
+    @inline(__always)
+    func lerp(vectorEnd: Vector3, interpolation: Float) -> Vector3 {
+        
+        return Vector3(value.0 + ((vectorEnd.value.0 - value.0) * interpolation),
+            value.1 + ((vectorEnd.value.1 - value.1) * interpolation),
+            value.2 + ((vectorEnd.value.2 - value.2) * interpolation))
+    }
+    
+    @inline(__always)
+    func crossProduct(rhs: Vector3) -> Vector3 {
+        
+        return Vector3(value.1 * rhs.value.2 - value.2 * rhs.value.1,
+            value.2 * rhs.value.0 - value.0 * rhs.value.2,
+            value.0 * rhs.value.1 - value.1 * rhs.value.0)
+    }
 }
 
 // MARK: - Darwin Support
